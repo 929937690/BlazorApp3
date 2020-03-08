d7ebbf23216677c1dcbe54fd1cde1f162666e30b
Yuntao Li <liy3@lopers.unk.edu>
2020-03-08 03:09:44 -0500

    Add project files.

---
+++ BlazorApp3/Server/Controllers/CoronaController.cs
@@ -0,0 +1,45 @@
+﻿using BlazorApp3.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorApp3.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoronaController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CoronaController> logger;
+
+        public CoronaController(ILogger<CoronaController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<CoronaData> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 2).Select(index => new CoronaData
+            {   
+                ProvinceState = Summaries[rng.Next(Summaries.Length)],
+                CountryRegion = Summaries[rng.Next(Summaries.Length)],
+                Confirmed = rng.Next(-20, 55),
+                Death = rng.Next(-20, 55),
+                Latitude = 30.9756,
+                Longtitude = 112.2707,
+                Summary = Summaries[rng.Next(Summaries.Length)]
+
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ BlazorApp3/Shared/CoronaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp3.Shared
+{
+    public class CoronaData
+    {
+        public string ProvinceState { get; set; }
+
+        public string CountryRegion { get; set; }
+
+        public int Confirmed { get; set; }
+
+        public int Death { get; set; }
+
+        public double Latitude { get; set; }
+        
+        public double Longtitude { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
